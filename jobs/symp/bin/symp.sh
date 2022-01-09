a4bf054d8598939b2ea619f97252f80ef4f6a8b8
Ruggero Valli <ruggerovalli@gmail.com>
2022-01-09 23:52:12 +0100

    implemented option for bash scripts

--- jobs/symp/bin/symp.sh
+++ jobs/symp/bin/symp.sh
@@ -1,25 +1,52 @@
 #/bin/bash
 
+# Define usage
+function usage {
+        echo "Usage: $(basename $0) [-cxs]" 2>&1
+        echo '   -c   compiles the program'
+        echo '   -x   executes the program'
+        echo '   -s   executes the scripts'
+        exit 1
+}
+
 # Find project path and job path
 cd `dirname $0`
 cd ..
 JOB_PATH="`pwd`"
-echo JOB PATH: $JOB_PATH
 cd ../../
 PROJECT_PATH="`pwd`"
-echo PROJECT PATH: $PROJECT_PATH
 
+while getopts ':cxs' OPTION; do
+    case "$OPTION" in
+    c)
+        # Compile
+        cd "$JOB_PATH"
+        c++ -I  "$PROJECT_PATH/lib/eigen-3.4.0"\
+            -I  "$PROJECT_PATH/src"\
+                "$PROJECT_PATH/src/"*.cpp\
+                src/main.cpp\
+                -o bin/main\
+            -lm\
+        && echo "Compilato!"
+        ;;
+    x)
+        # Execute program
+        cd "$JOB_PATH"
+        bin/mainsymp 3 "$JOB_PATH/output/prova.m"\
+        && echo "Eseguito!"
+        ;;
+    s)
+        # Execute scripts
+        cd "$JOB_PATH/bin"
+        matlab -r grafico_prova  -nosplash -nodesktop\
+        && echo "Script eseguito!"
+        ;;
+    ?)
+        usage
+        ;;
+    esac
+done
 
-# Compile
-cd "$JOB_PATH"
-c++ -I  "$PROJECT_PATH/lib/eigen-3.4.0"\
-    -I  "$PROJECT_PATH/src"\
-        "$PROJECT_PATH/src/"*.cpp\
-        src/main.cpp\
-        -o bin/main\
-    -lm
-echo "Compilato!"
-bin/mainsymp 3 "$JOB_PATH/output/prova.m"
-
-cd "$JOB_PATH"/bin
-matlab -r grafico_prova  -nosplash -nodesktop
+if [[ ${#} -eq 0 ]]; then
+   usage
+fi
